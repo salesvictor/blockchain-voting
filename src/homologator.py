23911e383154aa6aa334aa4f35b3419c2133dca0
jose.otavio.vidal <jose.otavio.vidal@gmail.com>
2019-12-01 00:57:41 -0300

    class homologator created

--- src/homologator.py
+++ src/homologator.py
@@ -1,33 +1,43 @@
 from block import Block, Transaction
 from blockchain import Blockchain
+from random import randint
 import api
 
-NUMBER_CANDIDATES = 0
+NUMBER_CANDIDATES = 1
+
+class Homologator():
+    def __init__(self, number_candidates: int):
+        self.dict = {'Candidate A': 0, 'Candidate B': 1, 'Candidate C': 2, 'Candidate D': 3, 'Candidate E': 4}
+        self.number_candidates = number_candidates
+        self.blockchain_candidates = []
+        for i in range(self.number_candidates):
+            bc = Blockchain()
+            self.blockchain_candidates.append(bc)
+
+    def map_vote(self, name_voter:str):
+        return self.dict[name_voter]
+
+    def get_candidate_name(self, position):
+        for name,val in self.dict.items():
+            if val == position:
+                return name
+
+    def add_vote(self, vote):
+        candidate_position = self.map_vote(vote.get_candidate_name())
+        self.blockchain_candidates[candidate_position].add_pending(vote.get_content())
+        self.blockchain_candidates[candidate_position].build_block()
+
+    def get_winner_election(self):
+        max = 0
+        name_candidate = ''
+        for blockchain_candidate in self.blockchain_candidates:
+            if max < len(blockchain_candidate.blockchain)-1:
+                max = len(blockchain_candidate.blockchain)
+                name_candidate = self.get_candidate_name(max)
+
+        return name_candidate
+
 
 if __name__ == "__main__":
 
-    blockchains = []
-
-    NUMBER_CANDIDATES = 5
-
-    for i in range(NUMBER_CANDIDATES):
-        bc = Blockchain()
-        blockchains.append(bc)
-
-    blockchains[0].add_pending(Transaction('my_address', 'your_address', 'Vidal', '001.001.002-56'))
-    blockchains[0].build_block()
-    blockchains[1].add_pending(Transaction('your_address', 'my_address', 'Batata', '001.001.003-56'))
-    blockchains[1].build_block()
-    blockchains[2].add_pending(Transaction('your_address', 'other_address', 'Lourescasjjdascvhasdjvcsdnço', '001.031.002-56'))
-    blockchains[2].build_block()
-    blockchains[3].add_pending(
-        Transaction('your_address', 'other_address', 'Lourescasjjdascvhasdjvcsdnço', '001.031.002-56'))
-    blockchains[3].build_block()
-    blockchains[4].add_pending(
-        Transaction('your_address', 'other_address', 'Lourescasjjdascvhasdjvcsdnço', '001.031.002-56'))
-    blockchains[4].build_block()
-
-    for blockchain in blockchains:
-        for block in blockchain.blockchain:
-            print(block)
-        print(len(blockchain.blockchain))
+    homolagator = Homologator(2)
